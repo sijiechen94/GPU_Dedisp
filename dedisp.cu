49925c8ea1389923354cb43826b995ccae83548e
souldew <kevinsouldew@gmail.com>
2014-07-09 16:33:34 +0800

    Stored OFFSETS in registers
    nvcc -arch=sm_20 -c 1.cu

--- dedisp.cu
+++ dedisp.cu
@@ -19,44 +19,116 @@
 //if your shared memory is smaller, make these smaller
 #define TILE_WIDTH_F 16
 #define TILE_WIDTH_T 768
+#define WARP 32
 
 __device__ __constant__ float c_DMs[1024];
 
 //For f in GHz, DM in pc*cm^-3
 #define OFFSET(f,DM) (int)rintf((4.149/f/f*DM)/dt)
+#define RELATIVE_OFFSET(ch) (OFFSET((((ch)+blockIdx.x*TILE_WIDTH_F)*bandwidth+f_bot),DM) - OFFSET(f_bot,DM))
+
 
 //The data is parallelised in the way that each block has every DM and covers few channels,
 //while each thread in a single block has a particular DM.
+//It seems that one block can hold only 1024 thread, so USE NO MORE THAN 1024 numDMs
 //Each thread goes through all t and sum their channels to add to global accumulator
 __global__ void timeshiftKernel(float* d_f_t, cufftComplex* d_dm_t, float f_bot, float bandwidth, 
 				int tsize, int numchan, float dt, float* c_DMs){
 
 	__device__ __shared__ float sharedInput[TILE_WIDTH_F][TILE_WIDTH_T];
+	float DM = c_DMs[threadIdx.x];
+	//The relative offset is quite small(<10000) so that every two of
+	//them is forced to be stored in one 32-bits register to reduce the use
+	//of registers.
+	int off_0_1 = 65536* RELATIVE_OFFSET(0) + RELATIVE_OFFSET(1);
+	int off_2_3 = 65536* RELATIVE_OFFSET(2) + RELATIVE_OFFSET(3);
+	int off_4_5 = 65536* RELATIVE_OFFSET(4) + RELATIVE_OFFSET(5);
+	int off_6_7 = 65536* RELATIVE_OFFSET(6) + RELATIVE_OFFSET(7);
+	int off_8_9 = 65536* RELATIVE_OFFSET(8) + RELATIVE_OFFSET(9);
+	int off_10_11 = 65536* RELATIVE_OFFSET(10) + RELATIVE_OFFSET(11);
+	int off_12_13 = 65536* RELATIVE_OFFSET(12) + RELATIVE_OFFSET(13);
+	int off_14_15 = 65536* RELATIVE_OFFSET(14) + RELATIVE_OFFSET(15);
 	
 	for ( int t=tsize; t>TILE_WIDTH_T; t-=TILE_WIDTH_T ){
 		
 		__syncthreads();
 		///Note that only the first 32 threads (first warp) is used to load,
 		///so make sure your data RUN AT LEAST 32 DMs
-		if(threadIdx.x<32)
-			for ( int i=0; i<384; i++ )	sharedInput[0][i*32+threadIdx.x] = 
-				d_f_t[(blockIdx.x*TILE_WIDTH_F + (i*32+threadIdx.x)/TILE_WIDTH_T)*tsize 
- 					+ ((i*32+threadIdx.x)%TILE_WIDTH_T ) + (t-TILE_WIDTH_T) ];
+		if(threadIdx.x<WARP)
+			for ( int i=0; i<TILE_WIDTH_T*TILE_WIDTH_F/WARP; i++ )	sharedInput[0][i*WARP+threadIdx.x] = 
+				d_f_t[(blockIdx.x*TILE_WIDTH_F + (i*WARP+threadIdx.x)/TILE_WIDTH_T)*tsize 
+ 					+ ((i*WARP+threadIdx.x)%TILE_WIDTH_T ) + (t-TILE_WIDTH_T) ];
 		__syncthreads();
 		
-		for ( int tj=0; tj<TILE_WIDTH_T; tj++ ) 
-			for ( int ch=0; ch<TILE_WIDTH_F; ch++){
-				///Note that atomicAdd may hinder performance, especially on old cards
-				atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-OFFSET((ch+blockIdx.x*TILE_WIDTH_F),(c_DMs[threadIdx.x])))
-					  ,sharedInput[0][tj+ch*TILE_WIDTH_F]);
-				///However, race conditions only happens between different BLOCKS,
-				///so if you are running only one block at any time, use normal add.
-				///And even you're running not too many (4 or 5) blocks, the chance
-				///of race conditions happening is quite negligible.
-				///PS: When you are feeling lucky, use normal add:
-				/***    (*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-OFFSET((ch+blockIdx.x*TILE_WIDTH_F),(c_DMs[threadIdx.x])))).x
-					+=sharedInput[0][tj+ch*TILE_WIDTH_F];    ***/
-			}
+		for ( int tj=0; tj<TILE_WIDTH_T; tj++ ) {
+
+			//This loop has been manually broken down to make sure that
+			//all the offsets are stored in registers, for once you use a
+			//loop, the compiler won't be able to determine the addresses
+			//at compile time so that the offsets must be settled in global
+			//memory and the load will be much slower. These code might be
+			//extremely ugly and hard to maintain but it's very important.
+			
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_0_1/65536)
+				  ,sharedInput[0][tj+0*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_0_1%65536)
+				  ,sharedInput[0][tj+1*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_2_3/65536)
+				  ,sharedInput[0][tj+2*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_2_3%65536)
+				  ,sharedInput[0][tj+3*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_4_5/65536)
+				  ,sharedInput[0][tj+4*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_4_5%65536)
+				  ,sharedInput[0][tj+5*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_6_7/65536)
+				  ,sharedInput[0][tj+6*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_6_7%65536)
+				  ,sharedInput[0][tj+7*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_8_9/65536)
+				  ,sharedInput[0][tj+8*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_8_9%65536)
+				  ,sharedInput[0][tj+9*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_10_11/65536)
+				  ,sharedInput[0][tj+10*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_10_11%65536)
+				  ,sharedInput[0][tj+11*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_12_13/65536)
+				  ,sharedInput[0][tj+12*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_12_13%65536)
+				  ,sharedInput[0][tj+13*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_14_15/65536)
+				  ,sharedInput[0][tj+14*TILE_WIDTH_F]);
+			atomicAdd((float*)(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_14_15%65536)
+				  ,sharedInput[0][tj+15*TILE_WIDTH_F]);
+
+			///Note that atomicAdd may hinder performance, especially on old cards
+			///However, race conditions only happens between different BLOCKS,
+			///so if you are running only one block at any time, use normal add.
+			///And even you're running not too many (4 or 5) blocks, the chance
+			///of race conditions happening is quite negligible.
+			///PS: When you are feeling lucky, use normal add:
+
+			/***  
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_0_1/65536)).x+=sharedInput[0][tj+0*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_0_1%65536)).x+=sharedInput[0][tj+1*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_2_3/65536)).x+=sharedInput[0][tj+2*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_2_3%65536)).x+=sharedInput[0][tj+3*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_4_5/65536)).x+=sharedInput[0][tj+4*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_4_5%65536)).x+=sharedInput[0][tj+5*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_6_7/65536)).x+=sharedInput[0][tj+6*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_6_7%65536)).x+=sharedInput[0][tj+7*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_8_9/65536)).x+=sharedInput[0][tj+8*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_8_9%65536)).x+=sharedInput[0][tj+9*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_10_11/65536)).x+=sharedInput[0][tj+10*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_10_11%65536)).x+=sharedInput[0][tj+11*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_12_13/65536)).x+=sharedInput[0][tj+12*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_12_13%65536)).x+=sharedInput[0][tj+13*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_14_15/65536)).x+=sharedInput[0][tj+14*TILE_WIDTH_F];
+			(*(d_dm_t+tsize*threadIdx.x+t-TILE_WIDTH_T+tj-off_14_15%65536)).x+=sharedInput[0][tj+15*TILE_WIDTH_F];
+			***/
+
+		}
 	};
 }
 
@@ -65,10 +137,8 @@ void dedispersion(float* f_t, int numchan, int tsize,
 
 	//Assume f_t is a 2D Array Input[f][t]
 
-	///Make Time Shift Plan
 	float sizeofDMArray = numDMs*sizeof(float);
 	cudaCheckErrors(cudaMemcpyToSymbol(c_DMs,DMs,sizeofDMArray,0,cudaMemcpyHostToDevice));
-	///Temporarily, no subbands involved
 
 	float* d_f_t;
 	cufftComplex* d_dm_t;
